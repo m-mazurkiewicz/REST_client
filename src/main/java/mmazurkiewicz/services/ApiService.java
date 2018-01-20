f7ff0a8cb809fd149790b5f3cf6677bddbb468fb
Marcin Mazurkiewicz <jaktam.3@gmail.com>
2018-01-20 17:00:05 +0100

    add Reactive methods

--- src/main/java/mmazurkiewicz/services/ApiService.java
+++ src/main/java/mmazurkiewicz/services/ApiService.java
@@ -1,9 +1,13 @@
 package mmazurkiewicz.services;
 
 import mmazurkiewicz.api.domain.User;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 public interface ApiService {
     List<User> getUsers(Integer limit);
+
+    Flux<User> getUsers(Mono<Integer> limit);
 }
